595bcb761b2bf61bc4c23d371c9b61646a2bc8b3
maryamtwahir <133905440+maryamtwahir@users.noreply.github.com>
2023-06-07 10:45:01 -0700

    Create Utils.java

---
+++ Utils.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+ 
+
+public class Utils {
+
+  private static Scanner scan = new Scanner(System.in);
+ 
+  public static String inputStr (String question) {
+    System.out.print (question);
+    return scan.nextLine();
+  }
+ 
+  public static int inputNum (String question) {
+    System.out.print (question);
+    int num = -1;
+    while (num <= 0) {
+      try {
+        String numStr = scan.nextLine();
+        num = Integer.parseInt(numStr);
+        num = 1;
+      } catch (Exception e) {
+        num = -1;
+      }
+      if (num <= 0)
+        System.out.println ("Please enter a valid phone number: ");
+    }
+    return num;
+  }
+}
